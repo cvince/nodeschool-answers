796d9d4f9b82c53d0ca830d73f46dfca67ad4231
Vincent Chan <cvince@Vincents-MacBook-Pro.local>
2014-01-10 03:12:43 -0800

    juggling-async done

--- juggling-async.js
+++ juggling-async.js
@@ -1,3 +1,26 @@
 var http = require('http');
 var bl = require('bl');
-var url = process.argv[2];
+var urlArr = process.argv;
+var urls = urlArr.slice(2, urlArr.length);
+
+var callbackDepth = 0;
+
+function processData (response){
+
+	response.pipe(bl(function (err, data){
+		
+		if (err) console.error(data);
+
+		var dataStr = data.toString();
+		console.log(dataStr);
+
+		if(callbackDepth<urls.length-1){
+			callbackDepth++;
+			http.get(urls[callbackDepth], processData)
+		}
+		
+	}))
+
+}
+
+http.get(urls[0], processData)
