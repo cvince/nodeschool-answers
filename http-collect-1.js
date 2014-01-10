a18298c0d2828811903a1751084cbebf7f4fea6d
Vincent Chan <cvince@Vincents-MacBook-Pro.local>
2014-01-10 02:43:05 -0800

    completed http-collect for both methods

--- http-collect-1.js
+++ http-collect-1.js
@@ -3,11 +3,22 @@ var url = process.argv[2];
 
 function processData (response){
 	
+	var dataArr = [];
+
 	function showData (data) {
-		console.log(data)
-	} 
+		dataArr.push(data);
+	}
+
+	function dataInfo (data) {
+		var dataStr = dataArr.join("");
+		var count = dataStr.length;
+		console.log(count);		
+		console.log(dataStr);
+	}
+
 	response.setEncoding('utf8');	
-	response.on("data", showData)
+	response.on("data", showData);
+	response.on("end", dataInfo);
 }
 
 http.get(url, processData);
--- http-collect-2.js
+++ http-collect-2.js
@@ -1,13 +1,22 @@
 var http = require('http');
+var bl = require('bl');
 var url = process.argv[2];
 
 function processData (response){
 	
-	function showData (data) {
-		console.log(data)
-	} 
-	response.setEncoding('utf8');	
-	response.on("data", showData)
+	// function showData (data) {
+	// 	console.log(data)
+	// } 
+	// response.setEncoding('utf8');	
+	// response.on("data", showData)
+
+	response.pipe(bl(function (err, data){
+		if (err) return err;
+		var dataStr = data.toString();
+		console.log(dataStr.length);
+		console.log(dataStr);
+	}))
+
 }
 
 http.get(url, processData);
