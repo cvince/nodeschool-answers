c0726136fb8a1ad3094e921ae6165de7f21a9438
Vincent Chan <vincent@dontspamme.com>
2014-01-12 22:36:54 -0800

    time-server.js complete

--- time-server.js
+++ time-server.js
@@ -1,9 +1,17 @@
-var net = require('net')
+var net = require('net');
 var port = Number(process.argv[2]);
 
-var server = net.createServer(function (socket) {
-	var data;
-	socket.write(data);
-    socket.end(data)
-})
-server.listen(port)
+var date = new Date();
+
+var server = net.createServer(function(socket) {
+	var year = date.getFullYear().toString();
+    var month = date.getMonth().toString()+1;     // starts at 0
+    var day = date.getDate().toString();    // returns the day of month
+    var hours = date.getHours().toString();
+    var minutes = date.getMinutes().toString();
+
+	socket.write(year+'-'+month+'-'+day+' '+hours+':'+minutes+'\n');
+	socket.end();
+});
+
+server.listen(port);
