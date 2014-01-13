16fc0fb5e387da72fd1ed568a127da6fcff286b2
vincent <vincent@vincentcchan.com>
2014-01-13 11:33:53 -0800

    cleared uppercaser

--- http-uppercaser.js
+++ http-uppercaser.js
@@ -1,11 +1,3 @@
 var http = require('http');
 var fs = require('fs');
 
-var port = Number(process.argv[2]);
-var file = process.argv[3];
-
-var server = http.createServer(function(req, res) {
-	fs.createReadStream(file).pipe(res);
-});
-
-server.listen(port);
