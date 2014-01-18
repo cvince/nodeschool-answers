94c9c2f45db24f4bffee4ed66612a82d7cb216a4
vincent <vincent@vincentcchan.com>
2014-01-17 21:07:32 -0800

    didnt understand uppercase at all

--- http-uppercaser.js
+++ http-uppercaser.js
@@ -1,3 +1,16 @@
 var http = require('http');
-var fs = require('fs');
+var map = require('through2-map');
 
+var server = http.createServer(function(req, res){
+
+	if(req.method == 'POST'){
+		req.pipe(map(function(input){
+			return input.toString().toUpperCase();
+		})).pipe(res);		
+	}else{
+		return res.end("need to send a post");
+	}
+
+})
+
+server.listen(process.argv[2]);
